ed48a5b428f76b553d06bff66f799305ec577682
--replace-all <kelvin_rizky_28rpl@student.smktelkom-mlg.sch.id>
2021-06-06 22:22:41 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Beranda from "./components/BMI.js";
+import Konversi from "./components/Conversion.js";
+import Daftarharibesar from "./components/Cicilan.js";
+import "./styles/styles.css";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+
+const element = (
+	<Router>
+		<nav className="navbar navbar-default">
+			<div className="container-fluid">
+				<ul className="nav navbar-nav">
+					<li><Link to="/">BMI</Link></li>
+					<li><Link to="/Konversi">Conversion</Link></li>
+					<li><Link to="/daftar">Cicilan</Link></li>
+				</ul>
+			</div>
+		</nav>                                                               
+		<div className="main-content">
+			<Switch>
+				<Route path="/daftar">
+					<Daftarharibesar />
+				</Route>
+				<Route path="/Konversi">
+					<Konversi />
+				</Route>
+				<Route path={["/beranda", "/"]}>
+					<Beranda />
+				</Route>
+			</Switch>
+		</div>
+	</Router>
+)
+
+ReactDOM.render(element,document.getElementById('app'));
